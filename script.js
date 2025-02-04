d2bdb51a8b0ac7281bd3411847fe5e323161ed98
AVG <pl.avgust@gmail.com>
2025-02-04 16:29:49 +0100

    Remove unused HTML and SCSS files to streamline project structure

--- script.js
+++ script.js
@@ -1,120 +1,53 @@
-const $tabsToDropdown = $(".tabs-to-dropdown");
+//Toggling Menu
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
 
-function generateDropdownMarkup(container) {
-  const $navWrapper = container.find(".nav-wrapper");
-  const $navPills = container.find(".nav-pills");
-  const firstTextLink = $navPills.find("li:first-child a").text();
-  const $items = $navPills.find("li");
-  const markup = `
-    <div class="dropdown d-md-none">
-      <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        ${firstTextLink}
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton"> 
-        ${generateDropdownLinksMarkup($items)}
-      </div>
-    </div>
-  `;
-  $navWrapper.prepend(markup);
+  if(toggle && nav) {
+      toggle.addEventListener('click', () => {
+          nav.classList.toggle('show');
+      })
+  }
 }
 
-function generateDropdownLinksMarkup(items) {
-  let markup = "";
-  items.each(function () {
-    const textLink = $(this).find("a").text();
-    markup += `<a class="dropdown-item" href="#">${textLink}</a>`;
-  });
+showMenu('nav-toggle', 'nav-menu');
 
-  return markup;
+//Toggling Active Link
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show');
 }
 
-function showDropdownHandler(e) {
-  // works also
-  //const $this = $(this);
-  const $this = $(e.target);
-  const $dropdownToggle = $this.find(".dropdown-toggle");
-  const dropdownToggleText = $dropdownToggle.text().trim();
-  const $dropdownMenuLinks = $this.find(".dropdown-menu a");
-  const dNoneClass = "d-none";
-  $dropdownMenuLinks.each(function () {
-    const $this = $(this);
-    if ($this.text() == dropdownToggleText) {
-      $this.addClass(dNoneClass);
-    } else {
-      $this.removeClass(dNoneClass);
-    }
-  });
-}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
-function clickHandler(e) {
-  e.preventDefault();
-  const $this = $(this);
-  const index = $this.index();
-  const text = $this.text();
-  $this.closest(".dropdown").find(".dropdown-toggle").text(`${text}`);
-  $this
-    .closest($tabsToDropdown)
-    .find(`.nav-pills li:eq(${index}) a`)
-    .tab("show");
-}
+// Scroll Reveal
 
-function shownTabsHandler(e) {
-  // works also
-  //const $this = $(this);
-  const $this = $(e.target);
-  const index = $this.parent().index();
-  const $parent = $this.closest($tabsToDropdown);
-  const $targetDropdownLink = $parent.find(".dropdown-menu a").eq(index);
-  const targetDropdownLinkText = $targetDropdownLink.text();
-  $parent.find(".dropdown-toggle").text(targetDropdownLinkText);
-}
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
 
-$tabsToDropdown.each(function () {
-  const $this = $(this);
-  const $pills = $this.find('a[data-toggle="pill"]');
+sr.reveal('.home-title', {} )
+sr.reveal('.button', {delay: 200} )
+sr.reveal('.home-img', {delay: 400} )
+sr.reveal('.home-social', {delay: 400,} )
 
-  generateDropdownMarkup($this);
+sr.reveal('.about-img', {} )
+sr.reveal('.about-subtitle', {delay: 200} )
+sr.reveal('.about-text', {delay: 400} )
 
-  const $dropdown = $this.find(".dropdown");
-  const $dropdownLinks = $this.find(".dropdown-menu a");
+sr.reveal('.skills-subtitle', {delay: 100} )
+sr.reveal('.skills-text', {delay: 150} )
+sr.reveal('.skills-data', {interval: 200} )
+sr.reveal('.skills-img', {delay: 400} )
 
-  $dropdown.on("show.bs.dropdown", showDropdownHandler);
-  $dropdownLinks.on("click", clickHandler);
-  $pills.on("shown.bs.tab", shownTabsHandler);
-});
-$( () => {
-	
-	//On Scroll Functionality
-	$(window).scroll( () => {
-		var windowTop = $(window).scrollTop();
-		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
-		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
-	});
-	
-	//Click Logo To Scroll To Top
-	$('#logo').on('click', () => {
-		$('html,body').animate({
-			scrollTop: 0
-		},500);
-	});
-	
-	//Smooth Scrolling Using Navigation Menu
-	$('a[href*="#"]').on('click', function(e){
-		$('html,body').animate({
-			scrollTop: $($(this).attr('href')).offset().top - 100
-		},500);
-		e.preventDefault();
-	});
-	
-	//Toggle Menu
-	$('#menu-toggle').on('click', () => {
-		$('#menu-toggle').toggleClass('closeMenu');
-		$('ul').toggleClass('showMenu');
-		
-		$('li').on('click', () => {
-			$('ul').removeClass('showMenu');
-			$('#menu-toggle').removeClass('closeMenu');
-		});
-	});
-	
-});
+sr.reveal('.work-img', {interval: 200} )
+
+sr.reveal('.contact-input', {interval: 200} )
